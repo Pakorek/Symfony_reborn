23abd320d7e34a9c383988c55e21836a9b47047b
pakorek <brice.darmenia@sfr.fr>
2021-08-17 00:58:43 +0200

    use ParamConverter

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Program;
+use App\Entity\Season;
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +16,22 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ProgramController extends AbstractController
 {
+    private $programRepo;
+
+    public function __construct(ProgramRepository $programRepo)
+    {
+        $this->programRepo = $programRepo;
+    }
+
     /**
      * Show all rows from Program's entity
      *
      * @Route("/", name="index")
-     * @param ProgramRepository $programRepo
      * @return Response
      */
-    public function index(ProgramRepository $programRepo): Response
+    public function index(): Response
     {
-        $programs = $programRepo->findAll();
+        $programs = $this->programRepo->findAll();
 
         if (!$programs) {
             throw $this->createNotFoundException(
@@ -37,16 +46,33 @@ class ProgramController extends AbstractController
     }
 
     /**
-     * @Route("/show/{id<\d+>}", name="show", methods={"GET"})
+     * @Route("/show/{program<\d+>}", name="show", methods={"GET"})
      *
-     * @param int $id
-     * @param ProgramRepository $programRepo
+     * @param Program $program
      * @return Response
      */
-    public function show(int $id, ProgramRepository $programRepo): Response
+    public function show(Program $program): Response
     {
-        $program = $programRepo->find($id);
+        $nbSeasons = count($program->getSeasons());
 
-        return $this->render('program/show.html.twig', ['program' => $program]);
+        return $this->render('program/show.html.twig', ['program' => $program, "nbSeasons" => $nbSeasons]);
+    }
+
+  /**
+   * @Route("/show/{program<\d+>}/seasons/{seasonId<\d+>}", name="season_show")
+   *
+   * @param Program $program
+   * @param int $seasonId
+   * @param SeasonRepository $seasonRepo
+   * @return Response
+   */
+    public function showSeason(Program $program, int $seasonId, SeasonRepository $seasonRepo)
+    {
+        $season = $seasonRepo->findOneBy([
+            "program_id" => $program->getId(),
+            "number" => $seasonId
+        ]);
+
+        return $this->render("program/showSeason.html.twig", ["program" => $program, "season" => $season]);
     }
 }
