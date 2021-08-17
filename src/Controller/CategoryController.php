d81ed6f33aa7d676f5056bf85b81234e2658e2c2
pakorek <brice.darmenia@sfr.fr>
2021-08-17 11:27:19 +0200

    create categoryType (form) and view associated

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/category", name="category_")
+ */
+class CategoryController extends AbstractController
+{
+  /**
+   * @Route("/", name="index")
+   * @param CategoryRepository $categoryRepo
+   * @return Response
+   */
+  public function index(CategoryRepository $categoryRepo): Response
+  {
+    // get all categories -> link to programs related to
+    $categories = $categoryRepo->findAll();
+
+    return $this->render('category/index.html.twig', ["categories" => $categories]);
+  }
+
+  /**
+   * @Route("/new", name="new")
+   *
+   * @param Request $request
+   * @return Response
+   */
+  public function new(Request $request): Response
+  {
+    $category = new Category();
+
+    $form = $this->createForm(CategoryType::class, $category);
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      //persist and flush
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($category);
+      $entityManager->flush();
+      // redirect to
+      return $this->redirectToRoute('category_index');
+    }
+
+    return $this->render('category/new.html.twig', ['form' => $form->createView()]);
+  }
+}
