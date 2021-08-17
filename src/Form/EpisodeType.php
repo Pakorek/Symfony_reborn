f10ad28f6fea7233058320532142bc5eed2e6d1a
pakorek <brice.darmenia@sfr.fr>
2021-08-17 13:50:05 +0200

    custom choice_label for new episode form

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -3,26 +3,36 @@
 namespace App\Form;
 
 use App\Entity\Episode;
+use App\Entity\Program;
+use App\Entity\Season;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EpisodeType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('title')
-            ->add('number')
-            ->add('synopsis')
-            ->add('season')
-        ;
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->add('title')
+      ->add('number')
+      ->add('synopsis')
+      ->add('season', EntityType::class, [
+        'class' => Season::class,
+        'choice_label' => function (Season $season) {
+          return $season->getProgram()->getTitle() . ': ' . $season->getNumber();
+        }]);
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Episode::class,
-        ]);
-    }
+  }
+
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $resolver->setDefaults([
+      'data_class' => Episode::class,
+    ]);
+  }
 }
